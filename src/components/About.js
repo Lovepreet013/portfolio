bf13d075c9f534eefd52b6d63fe5f971ed5085a0
Lovepreet Singh <104663771+Lovepreet013@users.noreply.github.com>
2024-01-06 14:34:03 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                 <h1>About Me</h1>
                 <div className='Aline'></div>
             </div>
-            <p>Hello! My name is Lovepreet and I enjoy creating things that live on the internet. My interest in web development started back in 2020 when I decided to try editing a website created by my brother — turns out hacking together a custom read more button taught me a lot about HTML & CSS!</p>
+            <p>Hello! My name is Lovepreet and I enjoy creating things that live on the internet. My interest in web development started back when I decided to try editing a website created by my brother — turns out hacking together a custom read more button taught me a lot about HTML & CSS!</p>
             <div>
                 <h3>Here are the some technologies I'm working with recently :</h3>
                 <div className='tech'>
